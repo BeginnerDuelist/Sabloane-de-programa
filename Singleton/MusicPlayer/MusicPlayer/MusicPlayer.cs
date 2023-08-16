571480b39d1f69a40e2a5f737e9441ff82cd32f0
BeginnerDuelist <100442245+BeginnerDuelist@users.noreply.github.com>
2023-08-16 11:13:41 +0300

    Add files via upload

---
+++ Singleton/MusicPlayer/MusicPlayer/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+     class MusicPlayer
+     {
+          private static MusicPlayer _instance = null!;
+          private static object locker = new object();
+
+          public int volume { get; set; }
+          private MusicPlayer()
+          {
+               Console.WriteLine("Music player is on!!!");
+          }
+          public static MusicPlayer GetInstance()
+          {
+               if (_instance == null)
+               {
+                    lock (locker)
+                    {
+                         if (_instance == null)
+                              _instance = new MusicPlayer();
+                    }
+               }
+               return _instance;
+          }
+          public void PlayMusic(string filename)
+          {
+               Console.WriteLine($"{filename}...");
+          }
+          public void StopMusic()
+          {
+               Console.WriteLine("Music Stoped");
+          }
+     }
+}
---
+++ Singleton/MusicPlayer/MusicPlayer/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton
+{
+     class Program
+     {
+          public static void Main(string[] args)
+          {
+               MusicPlayer.GetInstance().volume += 20;
+               MusicPlayer.GetInstance().PlayMusic("After Dark.mp3");
+               MusicPlayer.GetInstance().StopMusic();
+          }
+     }
+}
